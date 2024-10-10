a7b4f90851790158bc01994bc2a1491ff3c78780
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-10-10 11:26:17 +0200

    Flows -> Preparing the StateFlow.

--- app/src/main/java/com/example/todoapp/addtasks/ui/TasksViewModel.kt
+++ app/src/main/java/com/example/todoapp/addtasks/ui/TasksViewModel.kt
@@ -5,10 +5,27 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.todoapp.addtasks.domain.AddTaskUseCase
+import com.example.todoapp.addtasks.domain.GetTasksUseCase
+import com.example.todoapp.addtasks.ui.TasksUiState.Success
 import com.example.todoapp.addtasks.ui.model.TaskModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class TasksViewModel @Inject constructor():ViewModel() {
+class TasksViewModel @Inject constructor(
+    private val addTaskUseCase: AddTaskUseCase,
+    getTasksUiState: GetTasksUseCase
+):ViewModel() {
+
+    val uiState:StateFlow<TasksUiState> = getTasksUiState().map(::Success)
+        .catch{ TasksUiState.Error(it)}
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), TasksUiState.Loading)
 
     private val _showDialog = MutableLiveData<Boolean>()
     val showDialog: LiveData<Boolean> = _showDialog
@@ -23,6 +40,10 @@ class TasksViewModel @Inject constructor():ViewModel() {
     fun onTasksCreated(task:String){
         _showDialog.value = false
         _tasks.add(TaskModel(task= task))
+
+        viewModelScope.launch {
+            addTaskUseCase(TaskModel(task=task))
+        }
     }
     fun onShowDialogClick(){
         _showDialog.value = true
